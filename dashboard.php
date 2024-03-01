35f7d9d4b6837be203133e453cab8b3b20a7e78c
unknown <jintannaegi@gmail.com>
2024-03-01 19:33:53 +0800

    building dashboard

--- dashboard.php
+++ dashboard.php
@@ -1,7 +1,47 @@
 <?php
-    $title = "Dashboard";
-    include_once('header.php');
+$title = "Dashboard | MUTI";
+include_once('header.php');
 ?>
+
+
+<!-- Sub Header -->
+<div class="sub-header container">
+
+    <!-- Company Logo -->
+    <div class="company-logo-container flex space-between w-100">
+        <div class="company-logo">
+            <img src="assets/hondas.png" alt="Honda Logo">
+        </div>
+        <div class="company-logo">
+            <img src="assets/suzukis.png" alt="Suzuki Logo">
+        </div>
+        <div class="company-logo">
+            <img src="assets/yamahas.png" alt="Yamaha Logo">
+        </div>
+        <div class="company-logo">
+            <img src="assets/bajs.png" alt="Bajaj Logo">
+        </div>
+        <div class="company-logo">
+            <img src="assets/kawas.png" alt="Kawasaki Logo">
+        </div>
+    </div>
+
+    <!-- Search Bar and Other Buttons -->
+    <div class="search-bar">
+        <input type="text" placeholder="Search...">
+        <button><i class="fa-solid fa-magnifying-glass"></i></button>
+    </div>
+
+    <div class="btn-group">
+        <button>View All</button>
+        <button>Print Report</button>
+    </div>
+</div>
+
+<!-- Dashboard Page -->
+<div>
+    <div></div>
+</div>
 
 <?php
     // include_once('footer.php');
--- header.php
+++ header.php
@@ -1,21 +1,28 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="icon" type="image/x-icon" href="assets/web_icon.png" />
-    <title><?=$title?></title>
+    <title><?= $title ?></title>
 
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="fontawesome/css/all.min.css">
 </head>
+
 <body>
+    <!-- Website Header -->
     <header class="container">
+
+        <!-- MUTI Logo -->
         <div class="nav-logo">
             <div>
                 <img class="header-logo" src="assets/logo_header.png" alt="">
             </div>
         </div>
+
+        <!-- Navigation Area -->
         <a class="mobile-nav" href="#"><i class="fa-solid fa-bars"></i></a>
         <ul class="nav-group">
             <li class="nav-links"><a href="#">About <i class="fa-solid fa-caret-down"></i></a></li>
@@ -28,4 +35,7 @@
             <li class="nav-links"><a href="#">Branches</a></li>
         </ul>
         <a class="btn btn-contact" href="#">Contact Us</a>
+
+        <!-- Profile Area -->
+
     </header>
